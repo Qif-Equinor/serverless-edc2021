9575c0bc67323b32b376df5b779130e321a2a62b
Qiang Fu <qif@equinor.com>
2021-11-03 21:22:22 +0100

    add logging

--- aws-demo/handler.py
+++ aws-demo/handler.py
@@ -1,10 +1,13 @@
 import json
 import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def hello(event, context):
 
-    logging.info("AWS Lambda processing message from GitHub.")
+    logger.info(f"AWS Lambda processing message from GitHub: {event}.")
 
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
